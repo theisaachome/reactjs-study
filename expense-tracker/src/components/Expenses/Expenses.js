fc242a4e6ce919fb5bec5c32dd8da3d847573c56
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-05-20 20:16:25 +0630

    add render list

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -13,26 +13,14 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onFilterByYear={filterByYearHandler}></ExpensesFilter>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      {
+        props.expenses.map((expense)=>(
+        <ExpenseItem key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />))
+      }
     </Card>
   );
 };
