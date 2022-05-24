f0e18a8d31ed534b88fd7ba21c52d325efd6dd7c
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-05-24 22:11:14 +0630

    ref and portal

--- contact-app/src/components/UI/ErrorModal.js
+++ contact-app/src/components/UI/ErrorModal.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from "react-dom";
 import Button from './Button';
 import Card from './Card'
 import classes from "./ErrorModal.module.css";
-const ErrorModal = ({ title, message ,onConfirm}) => {
+
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+}
+const ModalOverlay = props => {
+    return <Card className={classes.modal}>
+        <header className={classes.header}>
+            <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+    </Card>
+}
+const ErrorModal = ({ title, message, onConfirm }) => {
     return (
         <>
-            <div className={classes.backdrop} onClick={onConfirm} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+            {
+            ReactDOM.createPortal(
+            <Backdrop onConfirm={onConfirm} />,
+             document.getElementById('backdrop-root'))
+             }
+             {
+                 ReactDOM.createPortal(
+                     <ModalOverlay
+                     title={title}
+                     message={message}
+                     onConfirm={onConfirm}/>,
+                     document.getElementById('overlay-root')
+                 )
+             }
         </>
     )
 }
